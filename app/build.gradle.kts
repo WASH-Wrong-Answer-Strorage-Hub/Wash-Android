1713db9c5a1abab7fce9e54c64d5c0f6d31ac874
changyunyeong <sally0109277@naver.com>
2024-07-29 16:38:18 +0900

    [BUILD] lottie animation implementation 추가 #4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+
+    // lottie 애니메이션
+    implementation ("com.airbnb.android:lottie:6.3.0")
 }
