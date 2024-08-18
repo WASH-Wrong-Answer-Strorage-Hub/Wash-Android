b824ea367a0a6e11aacbc893589f59aab3515a29
Daemon Jung <jjjssswww1006@gmail.com>
2024-08-18 17:14:30 +0900

    [feature] 문제 유형 관련 restful api 설정 #18

---
+++ app/src/main/java/com/wash/washandroid/presentation/fragment/category/CategoryApiService.kt
@@ -0,0 +1,30 @@
+package com.wash.washandroid.presentation.fragment.category
+
+import com.wash.washandroid.model.CategoryAddRequest
+import com.wash.washandroid.model.CategoryAddResponse
+import com.wash.washandroid.model.CategoryResponse
+import com.wash.washandroid.model.FolderResponse
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Query
+
+interface CategoryApiService {
+    @GET("/problems/types/1")
+    suspend fun getCategoryTypes(): Response<CategoryResponse>
+
+    @GET("/problems/types/2")
+    suspend fun getCategorySubfieldTypes(@Query("parentTypeId") parentTypeId: Int): Response<CategoryResponse>
+
+    @GET("/problems/types/3")
+    suspend fun getCategoryChapterTypes(@Query("parentTypeId") parentTypeId: Int): Response<CategoryResponse>
+
+    @GET("/folders")
+    suspend fun getCategoryFolderTypes(): Response<FolderResponse>
+
+    @POST("/problems/types")
+    suspend fun postCategoryTypes(
+        @Body request : CategoryAddRequest
+    ): Response<CategoryAddResponse>
+}
