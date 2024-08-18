96c0d8e8058e6fe0e8f1524f593448e0f251480c
Daemon Jung <jjjssswww1006@gmail.com>
2024-08-18 17:19:42 +0900

    [feature] 문제 유형 분류별 viewmodel 분리 작업 및 api 연결 #18

--- app/src/main/java/com/wash/washandroid/presentation/fragment/category/viewmodel/CategorySubjectViewModel.kt
+++ app/src/main/java/com/wash/washandroid/presentation/fragment/category/viewmodel/CategorySubjectViewModel.kt
@@ -1,4 +1,60 @@
 package com.wash.washandroid.presentation.fragment.category.viewmodel
 
-class CategorySubjectViewModel : CategoryChoiceViewModel() {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.wash.washandroid.R
+import com.wash.washandroid.model.CategoryType
+import com.wash.washandroid.presentation.fragment.category.CategoryApiService
+import com.wash.washandroid.presentation.fragment.category.NetworkModule
+import kotlinx.coroutines.launch
+
+open class CategorySubjectViewModel : ViewModel() {
+
+    private val _selectedButtonId = MutableLiveData<Int?>(null)
+    val selectedButtonId: LiveData<Int?> get() = _selectedButtonId
+
+    fun onButtonClicked(buttonId: Int) {
+        if (_selectedButtonId.value == buttonId) return
+        _selectedButtonId.value = buttonId
+    }
+
+    fun getButtonBackground(buttonId: Int): Int {
+        return when {
+            _selectedButtonId.value == buttonId && buttonId == R.id.category_add -> {
+                R.drawable.category_choice_add_clicked_btn
+            }
+            buttonId == R.id.category_add -> {
+                R.drawable.category_choice_add_btn
+            }
+            _selectedButtonId.value == buttonId -> {
+                R.drawable.category_choice_clicked_btn
+            }
+            else -> {
+                R.drawable.category_choice_btn
+            }
+        }
+    }
+
+    private val _categoryTypes = MutableLiveData<List<CategoryType>>()
+    val categoryTypes: LiveData<List<CategoryType>> get() = _categoryTypes
+
+    private val retrofit = NetworkModule.getClient()
+
+    // API 인터페이스를 생성
+    private val apiService: CategoryApiService = retrofit.create(CategoryApiService::class.java)
+
+    init {
+        fetchCategoryTypes()
+    }
+
+    private fun fetchCategoryTypes() {
+        viewModelScope.launch {
+            val response = apiService.getCategoryTypes()
+            if (response.isSuccessful && response.body()?.isSuccess == true) {
+                _categoryTypes.value = response.body()?.result?.types ?: emptyList()
+            }
+        }
+    }
 }
