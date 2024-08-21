acc8f43417b2a6ac8137807e5c696366bae6d4dd
Daemon Jung <jjjssswww1006@gmail.com>
2024-08-22 05:31:59 +0900

    [feature] 폴더 추가 api data class 추가 #18

--- app/src/main/java/com/wash/washandroid/model/CategoryFolderModels.kt
+++ app/src/main/java/com/wash/washandroid/model/CategoryFolderModels.kt
@@ -21,5 +21,34 @@ data class Folder(
     val folderId: Int,
 
     @SerializedName("folderName")
-    val folderName: String
+    val folderName: String,
+
+    @SerializedName("orderValue")
+    val orderValue: Int
+)
+
+data class PostFolderResponse(
+    @SerializedName("isSuccess")
+    val isSuccess: Boolean,
+
+    @SerializedName("code")
+    val code: Int,
+
+    @SerializedName("message")
+    val message: String,
+
+    @SerializedName("result")
+    val result: List<PostFolder>
+)
+
+data class PostFolder(
+    @SerializedName("message")
+    val message: String,
+
+    @SerializedName("folderId")
+    val folderId: Int
+)
+
+data class PostFolderRequest(
+    var folderName: String
 )
