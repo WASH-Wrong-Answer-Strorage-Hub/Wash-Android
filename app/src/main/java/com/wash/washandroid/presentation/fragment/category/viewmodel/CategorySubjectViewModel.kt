84df6831824aaf91abf3ad1d653a5d736a2d5c9a
Daemon Jung <jjjssswww1006@gmail.com>
2024-08-22 05:32:18 +0900

    [delete] 불필요한 import 취소 #18

--- app/src/main/java/com/wash/washandroid/presentation/fragment/category/viewmodel/CategorySubjectViewModel.kt
+++ app/src/main/java/com/wash/washandroid/presentation/fragment/category/viewmodel/CategorySubjectViewModel.kt
@@ -6,7 +6,6 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.wash.washandroid.R
-import com.wash.washandroid.model.CategorySubjectAddRequest
 import com.wash.washandroid.model.CategoryType
 import com.wash.washandroid.presentation.fragment.category.network.CategoryApiService
 import com.wash.washandroid.presentation.fragment.category.network.NetworkModule
@@ -64,24 +63,6 @@ open class CategorySubjectViewModel : ViewModel() {
                 }
             } catch (e: Exception) {
                 Log.e("CategorySubjectViewModel", "Error fetching category types: ${e.message}")
-            }
-        }
-    }
-
-    // 과목 추가 API 호출 함수
-    fun addCategoryType(categoryTypeName: String, typeLevel: Int) {
-        viewModelScope.launch {
-            try {
-                val response = apiService.postCategoryTypes(CategorySubjectAddRequest(categoryTypeName, typeLevel))
-                if (response.isSuccessful && response.body()?.isSuccess == true) {
-                    // 성공적으로 과목이 추가된 경우, 리스트를 다시 가져옴
-                    fetchCategoryTypes()
-                } else {
-                    Log.e("CategorySubjectViewModel", "Failed to add category type: ${response.errorBody()?.string()}")
-                }
-            } catch (e: Exception) {
-                // 에러 처리
-                Log.e("CategorySubjectViewModel", "Error adding category type: ${e.message}")
             }
         }
     }
