6dfd3a04e0a2eac37461797be72e086e20474af5
Daemon Jung <jjjssswww1006@gmail.com>
2024-08-26 03:58:19 +0900

    [chore] base url 변경 #59

--- app/src/main/java/com/wash/washandroid/presentation/fragment/home/NetworkModule.kt
+++ app/src/main/java/com/wash/washandroid/presentation/fragment/home/NetworkModule.kt
@@ -17,7 +17,6 @@ import retrofit2.Response
 object NetworkModule {
 
     private const val BASE_URL = "https://dev.team-wash.store"
-    //private const val BASE_URL = "http://192.168.35.23:3000"
 
     private var retrofit: Retrofit? = null
 
