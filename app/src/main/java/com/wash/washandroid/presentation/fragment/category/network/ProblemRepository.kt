018c0cc94fd4fcbe789c3bc264a10f67a92ddbc9
Daemon Jung <jjjssswww1006@gmail.com>
2024-08-30 03:26:46 +0900

    [chore] 문제 api 수정 #59

--- app/src/main/java/com/wash/washandroid/presentation/fragment/category/network/ProblemRepository.kt
+++ app/src/main/java/com/wash/washandroid/presentation/fragment/category/network/ProblemRepository.kt
@@ -1,76 +1,31 @@
 package com.wash.washandroid.presentation.fragment.category.network
 
-import android.util.Log
+import com.wash.washandroid.presentation.fragment.chat.ChatApiService
+import com.wash.washandroid.presentation.fragment.chat.PostChatRoomRequest
+import com.wash.washandroid.presentation.fragment.chat.PostChatRoomResponse
+import com.wash.washandroid.presentation.fragment.problem.network.EditProblemRequest
+import com.wash.washandroid.presentation.fragment.problem.network.EditProblemResponse
+import com.wash.washandroid.presentation.fragment.problem.network.PostProblemRequest
 import com.wash.washandroid.presentation.fragment.problem.network.PostProblemResponse
 import com.wash.washandroid.presentation.fragment.problem.network.ProblemApiService
-import okhttp3.MediaType.Companion.toMediaTypeOrNull
-import okhttp3.MultipartBody
-import okhttp3.RequestBody.Companion.asRequestBody
-import okhttp3.RequestBody.Companion.toRequestBody
 import retrofit2.Response
-import java.io.File
 
 class ProblemRepository {
 
     private val retrofit = NetworkModule.getClient()
     private val apiService: ProblemApiService = retrofit.create(ProblemApiService::class.java)
+    private val chatService: ChatApiService = retrofit.create(ChatApiService::class.java)
 
-    suspend fun postProblem(
-        problemImageFile: File?,
-        solutionImageFiles: List<File>,
-        passageImageFiles: List<File>,
-        additionalImageFiles: List<File>,
-        jsonData: String
-    ): Response<PostProblemResponse> {
 
-        Log.d("postProblem", "Problem Image File: ${problemImageFile?.name}")
-        Log.d("postProblem", "Solution Image Files: ${solutionImageFiles.map { it.name }}")
-        Log.d("postProblem", "Passage Image Files: ${passageImageFiles.map { it.name }}")
-        Log.d("postProblem", "Additional Image Files: ${additionalImageFiles.map { it.name }}")
-        Log.d("postProblem", "JSON Data: $jsonData")
+    suspend fun postProblem(request: PostProblemRequest): Response<PostProblemResponse> {
+        return apiService.postProblem(request)
+    }
 
-        val problemImagePart = problemImageFile?.let {
-            MultipartBody.Part.createFormData(
-                name = "problemImage",
-                filename = it.name,
-                body = it.asRequestBody("image/png".toMediaTypeOrNull())
-            )
-        }
+    suspend fun editProblem(request: EditProblemRequest): Response<EditProblemResponse> {
+        return apiService.editProblem(request)
+    }
 
-        val solutionImageParts = solutionImageFiles.map {
-            MultipartBody.Part.createFormData(
-                name = "solutionImages",
-                filename = it.name,
-                body = it.asRequestBody("image/png".toMediaTypeOrNull())
-            )
-        }
-
-        val passageImageParts = passageImageFiles.map {
-            MultipartBody.Part.createFormData(
-                name = "passageImages",
-                filename = it.name,
-                body = it.asRequestBody("image/png".toMediaTypeOrNull())
-            )
-        }
-
-        val additionalImageParts = additionalImageFiles.map {
-            MultipartBody.Part.createFormData(
-                name = "additionalImages",
-                filename = it.name,
-                body = it.asRequestBody("image/png".toMediaTypeOrNull())
-            )
-        }
-
-        val dataPart = jsonData.toRequestBody("application/json".toMediaTypeOrNull())
-
-        Log.d("postProblem", "Request Data Prepared")
-
-        return apiService.postProblem(
-            problemImage = problemImagePart,
-            solutionImages = solutionImageParts,
-            passageImages = passageImageParts,
-            additionalImages = additionalImageParts,
-            data = dataPart
-        )
+    suspend fun getChatrooms(request: PostChatRoomRequest): Response<PostChatRoomResponse> {
+        return chatService.getChatrooms(request)
     }
 }
