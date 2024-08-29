11a1fc4f3be25eeb342f4f699d7c58030c18d664
Daemon Jung <jjjssswww1006@gmail.com>
2024-08-30 03:29:27 +0900

    [chore] api 변경에 따른 viewmodel 의존성 주입 #59

--- app/src/main/java/com/wash/washandroid/presentation/fragment/category/viewmodel/CategoryFolderViewModelFactory.kt
+++ app/src/main/java/com/wash/washandroid/presentation/fragment/category/viewmodel/CategoryFolderViewModelFactory.kt
@@ -3,6 +3,9 @@ package com.wash.washandroid.presentation.fragment.category.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.wash.washandroid.presentation.fragment.category.network.ProblemRepository
+import com.wash.washandroid.presentation.fragment.chat.ChatViewModel
+import com.wash.washandroid.presentation.fragment.problem.add.ProblemAnswerViewModel
+import com.wash.washandroid.presentation.fragment.problem.old.ProblemInfoViewModel
 
 class CategoryFolderViewModelFactory(
     private val repository: ProblemRepository
@@ -11,6 +14,15 @@ class CategoryFolderViewModelFactory(
         if (modelClass.isAssignableFrom(CategoryFolderViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
             return CategoryFolderViewModel(repository) as T
+        } else if (modelClass.isAssignableFrom(ProblemInfoViewModel::class.java)){
+            @Suppress("UNCHECKED_CAST")
+            return ProblemInfoViewModel(repository) as T
+        } else if (modelClass.isAssignableFrom(ProblemAnswerViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return ProblemAnswerViewModel(repository) as T
+        } else if (modelClass.isAssignableFrom(ChatViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return ChatViewModel(repository) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
