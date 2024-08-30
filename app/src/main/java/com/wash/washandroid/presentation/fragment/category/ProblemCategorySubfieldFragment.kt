53498cfb659f7957f2dc9bb5d98a6d36adf08b27
Daemon Jung <jjjssswww1006@gmail.com>
2024-08-30 21:00:13 +0900

    [feature] ProblemManager에 선택유형값 저장 #59

--- app/src/main/java/com/wash/washandroid/presentation/fragment/category/ProblemCategorySubfieldFragment.kt
+++ app/src/main/java/com/wash/washandroid/presentation/fragment/category/ProblemCategorySubfieldFragment.kt
@@ -10,12 +10,12 @@ import android.view.ViewGroup
 import android.view.animation.AccelerateDecelerateInterpolator
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.wash.washandroid.R
 import com.wash.washandroid.databinding.FragmentProblemCategorySubfieldBinding
+import com.wash.washandroid.presentation.base.MainActivity
 import com.wash.washandroid.presentation.fragment.category.adapter.CategorySubfieldAdapter
 import com.wash.washandroid.presentation.fragment.category.dialog.CategorySubfieldDialog
 import com.wash.washandroid.presentation.fragment.category.network.ProblemRepository
@@ -23,6 +23,8 @@ import com.wash.washandroid.presentation.fragment.category.viewmodel.CategoryFol
 import com.wash.washandroid.presentation.fragment.category.viewmodel.CategoryFolderViewModelFactory
 import com.wash.washandroid.presentation.fragment.category.viewmodel.CategorySubfieldViewModel
 import com.wash.washandroid.presentation.fragment.category.viewmodel.CategoryViewModel
+import com.wash.washandroid.presentation.fragment.problem.add.ProblemAddViewModel
+import com.wash.washandroid.presentation.fragment.problem.add.ProblemManager
 import com.wash.washandroid.utils.CategoryItemDecoration
 
 class ProblemCategorySubfieldFragment : Fragment() {
@@ -32,11 +34,12 @@ class ProblemCategorySubfieldFragment : Fragment() {
     private val binding: FragmentProblemCategorySubfieldBinding
         get() = requireNotNull(_binding){"FragmentProblemCategorySubfieldBinding -> null"}
     private val categoryViewModel: CategoryViewModel by activityViewModels()
-    private val categorySubfieldViewModel: CategorySubfieldViewModel by viewModels()
+    private val categorySubfieldViewModel: CategorySubfieldViewModel by activityViewModels()
     private val categoryFolderViewModel: CategoryFolderViewModel by activityViewModels {
         val problemRepository = ProblemRepository()
         CategoryFolderViewModelFactory(problemRepository)
     }
+    private val problemAddViewModel: ProblemAddViewModel by activityViewModels()
 
     private var categorySubfieldDialog: CategorySubfieldDialog? = null
 
@@ -95,13 +98,11 @@ class ProblemCategorySubfieldFragment : Fragment() {
                     putInt("selectedSubfieldTypeId", typeId)
                     apply()
                 }
+                val currentIndex = problemAddViewModel.currentIndex.value ?: 0
+                ProblemManager.updateMidTypeProblemData(currentIndex, typeId)
                 categoryFolderViewModel.setMidTypeId(typeId)
                 navController.navigate(R.id.action_navigation_problem_category_subfield_to_chapter_fragment, bundle)
             }
-        }
-
-        binding.skipBtn.setOnClickListener {
-            navController.navigate(R.id.action_navigation_problem_category_subject_to_folder_fragment)
         }
 
         binding.categoryBackBtn.setOnClickListener {
